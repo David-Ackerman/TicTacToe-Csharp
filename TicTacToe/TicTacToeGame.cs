60515ff66c555fc91cd45c78179d7f17c5ff7f16
David_Ackerman <david_pereira-16@hotmail.com>
2022-05-24 00:08:32 -0300

    Add cpu player to tic tac toe game

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -29,20 +29,12 @@ namespace TicTacToe.src
                 this.boardMatrix[i] = " ";
             }
 
+            //Inicia em paralelo as threads do game contra IA (thread do jogador, Cpu, Campo e verificar vencedor)
+            Parallel.Invoke(PlayerMove, IaMove, CheckWinner, Board);
 
-            // Cria as threads dos metodos usados no jogo da velha (thread do jogador A, jogador B, Campo e verificar vencedor)
-            Thread board = new(new ThreadStart(Board));
-            Thread playerA = new(new ThreadStart(PlayerA));
-            Thread playerB = new(new ThreadStart(PlayerB));
-            Thread checkWinner = new(new ThreadStart(CheckWinner));
 
-            //Inicia as threads
-            board.Start();
-            playerA.Start();
-            playerB.Start();
-            checkWinner.Start();
         }
-        private void PlayerA()
+        private void PlayerMove()
         {
             // Lopping para a thread ficar executando enquanto o partida não termina
             while (game)
@@ -51,28 +43,29 @@ namespace TicTacToe.src
                 if (turn == 0)
                 {
                     // Verifica se o jogador 1 fez sua jogada para prosseguir com as etapas do jogo
-                    if (Play(1))
-                    {                        
+                    if (Play())
+                    {
                         turn = 1;
                     };
-                }else
+                }
+                else
                 {
                     Thread.Sleep(500);
 
                 }
             }
         }
-        private void PlayerB()
+        private void IaMove()
         {
             // Lopping para a thread ficar executando enquanto o partida não termina
 
             while (game)
             {
-                // verifica se é a vez do jogador 2
+                // verifica se é a vez da CPU
                 if (turn == 3)
                 {
-                    // Verifica se o jogador 1 fez sua jogada para prosseguir com as etapas do jogo
-                    if (Play(2))
+                    // Verifica se a cpu fez sua jogada para prosseguir com as etapas do jogo
+                    if (IaPlay())
                     {
                         turn = 4;
                     };
@@ -114,7 +107,7 @@ namespace TicTacToe.src
                     if (this.VerifyPlay("0"))
                     {
                         game = false;
-                        Console.WriteLine("Jogador 2 ganhou");
+                        Console.WriteLine("CPU ganhou");
                     }
                     else
                     {
@@ -139,7 +132,7 @@ namespace TicTacToe.src
                 if (turn == 1 || turn == 4)
                 {
                     Console.Clear(); // Limpa o console, pra evitar puluição visual
-                    string board = "| " ; // Inicia a string que será usada para representar o campo
+                    string board = "| "; // Inicia a string que será usada para representar o campo
 
                     // Laço for usado para verificar cada item do array de jogadas
                     for (int i = 0; i < 9; i++)
@@ -155,7 +148,7 @@ namespace TicTacToe.src
 
                     // Imprime o campo com as jogadas feitas, e passa o turno;
                     Console.WriteLine(board);
-                    turn++; 
+                    turn++;
 
                 }
                 Thread.Sleep(500);
@@ -163,12 +156,12 @@ namespace TicTacToe.src
         }
 
         // Metódo da jogada, faz a leitura do valor digitado pelo jogador e adiciona ao array de jogadas
-        private bool Play(int player)
+        private bool Play()
         {
 
             Console.WriteLine("Digite a posição do tabuleiro (1 a 9): ");
             int pos = readInt(); // Recebe o Valor digitado pelo jogador através do método readInt;
-            
+
             // Verifica se a posição escolhida pelo jogador está livre e se a posição está entre 1 e 9, se não for uma posição valida será solicitada uma nova escolha de posição
             while (pos < 1 || pos > 9 || boardMatrix[pos - 1] != " ")
             {
@@ -176,9 +169,7 @@ namespace TicTacToe.src
                 pos = readInt();
             }
 
-            
-            string character = player == 1 ? "x" : "0"; // Verifica qual é o jogador para definir qual caracter usar na jogada dele (x ou 0)
-            boardMatrix[pos - 1] = character; // Adiciona o caracter do jogador no array de jogadas, (pos - 1 porque o index do array começa em 0, e as jogadas são de 1 a 9, precisa subtrair 1)
+            boardMatrix[pos - 1] = "x"; // Adiciona o caracter do jogador no array de jogadas, (pos - 1 porque o index do array começa em 0, e as jogadas são de 1 a 9, precisa subtrair 1)
             return true; // Retorno para indicar que a jogada foi feita
         }
 
@@ -220,7 +211,7 @@ namespace TicTacToe.src
             if (plays.Contains(2) && plays.Contains(4) && plays.Contains(6)) return true; // Verifica se ganhou marcando a diagonal esquerda inferior até direita superio
 
             // Verifica se ainda tem jogadas disponiveis, se não tiver ocorreu um empate
-            if(!boardMatrix.Contains(" "))
+            if (!boardMatrix.Contains(" "))
             {
                 game = false;
                 Console.WriteLine("Empatou");
@@ -229,6 +220,118 @@ namespace TicTacToe.src
 
         }
 
+        private Boolean IaPlay()
+        {
+            List<int> playerPlays = new(); //Inicia uma lista de tamanho dinamico que vai ser preenchida com as jogadas de apenas um jogador
+            List<int> cpuPlays = new(); //Inicia uma lista de tamanho dinamico que vai ser preenchida com as jogadas de apenas um jogador
+            List<int> voidPlays = new(); //Inicia uma lista de tamanho dinamico que vai ser preenchida com as jogadas de apenas um jogador
+            int play = -1;
+            // Percorre o array de jogadas e adiciona as jogadas do jogador para a nova lista
+            for (int i = 0; i < 9; i++)
+            {
+                if (this.boardMatrix[i] == "x")
+                {
+                    playerPlays.Add(i);
+                }
+                else if (this.boardMatrix[i] == "0")
+                {
+                    cpuPlays.Add(i);
+
+                }
+                else
+                {
+                    voidPlays.Add(i);
+                }
+            }
+            Console.WriteLine(playerPlays);
+            Console.WriteLine(cpuPlays);
+            Console.WriteLine(voidPlays);
+
+
+            //attack 
+            if (cpuPlays.Count >= 2)
+            {
+                play = IaPlayDecision(cpuPlays, voidPlays);
+                if (play >= 0 && play < 9)
+                {
+                    this.boardMatrix[play] = "0";
+                    return true;
+
+                }
+            }
+            if (playerPlays.Count >= 2)
+            {
+                play = IaPlayDecision(playerPlays, voidPlays);
+                if (play >= 0 && play < 9)
+                {
+                    this.boardMatrix[play] = "0";
+                    return true;
+
+                }
+
+            }
+            var random = new Random();
+            this.boardMatrix[voidPlays[random.Next(voidPlays.Count)]] = "0";
+
+            return true;
+        }
+
+        private int IaPlayDecision(List<int> plays, List<int> voidPlays)
+        {
+
+            if (voidPlays.Contains(0))
+            {
+                if (plays.Contains(1) && plays.Contains(2)) return 0;
+                if (plays.Contains(3) && plays.Contains(6)) return 0;
+                if (plays.Contains(4) && plays.Contains(8)) return 0;
+            }
+            if (voidPlays.Contains(1))
+            {
+                if (plays.Contains(0) && plays.Contains(2)) return 1;
+                if (plays.Contains(4) && plays.Contains(7)) return 1;
+            }
+            if (voidPlays.Contains(2))
+            {
+                if (plays.Contains(0) && plays.Contains(1)) return 2;
+                if (plays.Contains(5) && plays.Contains(8)) return 2;
+                if (plays.Contains(4) && plays.Contains(6)) return 2;
+            }
+            if (voidPlays.Contains(3))
+            {
+                if (plays.Contains(4) && plays.Contains(5)) return 3;
+                if (plays.Contains(1) && plays.Contains(6)) return 3;
+            }
+            if (voidPlays.Contains(4))
+            {
+                if (plays.Contains(3) && plays.Contains(5)) return 4;
+                if (plays.Contains(1) && plays.Contains(7)) return 4;
+                if (plays.Contains(0) && plays.Contains(8)) return 4;
+                if (plays.Contains(2) && plays.Contains(6)) return 4;
+            }
+            if (voidPlays.Contains(5))
+            {
+                if (plays.Contains(3) && plays.Contains(4)) return 5;
+                if (plays.Contains(2) && plays.Contains(8)) return 5;
+            }
+            if (voidPlays.Contains(6))
+            {
+                if (plays.Contains(7) && plays.Contains(8)) return 6;
+                if (plays.Contains(0) && plays.Contains(3)) return 6;
+                if (plays.Contains(2) && plays.Contains(4)) return 6;
+            }
+            if (voidPlays.Contains(7))
+            {
+                if (plays.Contains(6) && plays.Contains(8)) return 7;
+                if (plays.Contains(1) && plays.Contains(4)) return 7;
+            }
+            if (voidPlays.Contains(8))
+            {
+                if (plays.Contains(6) && plays.Contains(7)) return 8;
+                if (plays.Contains(2) && plays.Contains(5)) return 8;
+                if (plays.Contains(0) && plays.Contains(4)) return 8;
+            }
+            return 9;
+        }
 
     }
 }
